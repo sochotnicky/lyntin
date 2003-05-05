04d5a50f96d1ae89869bf8b91f34b97c5804fcad
willhelm <>
2003-05-05 05:56:34 +0000

    Re-Adding.

---
+++ lyntin/modules/testsystem/__init__.py
@@ -0,0 +1,11 @@
+#######################################################################
+# This file is part of Lyntin
+# copyright (c) Free Software Foundation 2001, 2002
+#
+# Lyntin is distributed under the GNU General Public License license.  See the
+# file LICENSE for distribution details.
+# $Id: __init__.py,v 1.1 2003/05/05 05:56:34 willhelm Exp $
+#######################################################################
+"""
+This directory contains the testmodule, tests, and benchmarks.
+"""
