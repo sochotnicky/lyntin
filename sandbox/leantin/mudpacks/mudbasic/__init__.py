5e73e9b6e9c8f03f80faad957294bc74ee951283
odbrazen <>
2004-04-20 16:54:02 +0000

    * added 'log_dir' member to Login objects * added 'prompt' and 'strip_prompt' to Mud objects

--- sandbox/leantin/mudpacks/mudbasic/__init__.py
+++ sandbox/leantin/mudpacks/mudbasic/__init__.py
@@ -1,7 +1,8 @@
 
 class Player(object):
   """A boring object that just stores other stuff"""
-
+  log_dir = None
+  
   def __init__(self): pass
   def sess_init(self, sess):
     """called after the session has been set up,
@@ -14,7 +15,9 @@ class Mud(object):
   name = None
   name_prompt = None
   pass_prompt = None
-  
+
+  prompt = ''
+  strip_prompt = 0
   def __init__(self): pass
   def sess_init(self, sess):
     """called after the session has been set up,
