f8622e5da8cd03d1ca61edad8923064565b77e4a
odbrazen <>
2004-04-20 17:17:05 +0000

    * use libmisc.Flag objects, renamed from Shutdown * reading thread was moved to session.py

--- sandbox/leantin/engine.py
+++ sandbox/leantin/engine.py
@@ -21,19 +21,17 @@ class Engine(object):
     self.ui = {}
     self.sessions = []
     self._default_session = None
-    self.shutdown = libmisc.Shutdown()
+    self.shutdown = libmisc.Flag()
     return
 
   def add_session(self, sess):
     if (not self._default_session):
-      print "Assiging default session"
       self._default_session = sess
     else:
-      print "Assiging regular session"
       self.setup_reads_for_session(sess)
 
     self.sessions.insert(0, sess) # newest sessions come first
-    self.shutdown.also_shutdown(sess.shutdown)
+    self.shutdown.also_flag(sess.shutdown)
      # create the first ocatch that handles commands
     cmd = mudcommands.CommandCatcher(listen=1, muffle=1)
     cmd.add_callback(lambda :cmd.apply_command(sess))
@@ -51,12 +49,12 @@ class Engine(object):
     return
 
   def rm_session(self, sess):
-    if (sess == self._default_session):
+    if (sess == self._default_session): # sanity check
       return
     
     self.sessions = filter(lambda x:x!=sess, self.sessions)
     self.ui.change_session(self.sessions[0].ui)
-    sess.ui._destroy() # we have to hide/unhide others first or packing gets messed up    
+    sess.ui._destroy() # we have to hide/unhide others first or packing gets messed up
     return
 
   def ch_session(self, sess):
@@ -67,45 +65,16 @@ class Engine(object):
     return
 
   def setup_reads_for_session(self, sess):
-    lb = libmisc.LineBuffer()
-    def from_mud():
-      while not sess.shutdown:
-        try:
-          txt = sess.sock.read()
-        except: # bad things, just get out
-          self.shutdown.shutdown()
-          return # finishes the tread
-        if (txt):
-          for (line) in lb.add(txt):
-            final_line = sess.icatch.input(line)
-            sess.ui.write(final_line)
-          if (lb.pending and sess.rawcatch):
-            for (i, rawcatcher) in enumerate(sess.rawcatch):
-              output = rawcatcher.raw_input(lb.pending)
-              if (output):
-                sess.sock.write(output+"\n")
-                sess.rawcatch.pop(i)
-      print "Shutting down sock", sess.name
-    
-    junk_t = threading.Thread(target=from_mud)
+    junk_t = threading.Thread(target=sess.from_mud)
     junk_t.start()
 
   def to_mud(self, text):
-    if (self.sessions):
-      sess = self.sessions[0]
-    else:
-      sess = self._default_session
-
-    final_text = sess.ocatch.input(text)
-    if (final_text):
-      if (sess.sock and not sess.sock.shutdown):
-        sess.sock.write(final_text)
-      else:
-        sess.ui.write("# WARNING, output ignored.  You aren't attached to anything\n")
+    sess = self.sessions[0]
+    sess.to_mud(text)
 
 class Quit(mudcommands.Command):
   command = 'end'
   def do_command(sess, arg):
     global engine
-    engine.shutdown.shutdown()
+    engine.shutdown.flag_true()
     return
