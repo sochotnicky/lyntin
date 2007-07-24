725cc6fea8f0297fbf406975ebed99ab1567236a
willhelm <>
2007-07-24 00:21:40 +0000

    Updated documentation, added setuptools support.

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@
 from distutils.core import setup
 import re, sys, lyntin
 
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
 VERSION = str(lyntin.__version__)
 (AUTHOR, EMAIL) = re.match("^(.*?)\s*<(.*)>$", lyntin.__author__).groups()
 URL = lyntin.__url__
@@ -16,14 +20,46 @@ else:
 setup(name="lyntin",
       version=VERSION.lower(),
       description="Lyntin mud client",
+      long_description = """
+Lyntin
+======
+
+Summary
+-------
+
+Lyntin is a mud client written in Python that sports multiple
+user interfaces (tk, text, curses) and runs on multiple platforms
+(Windows, Linux, Mac OSX, ...).  It has a module system that allows
+users to write new Lyntin commands, modify default Lyntin behavior,
+and extend the client to suit their individual needs.
+
+Download and installation
+-------------------------
+
+To download and install PyBlosxom you can go to::
+
+   http://lyntin.sourceforge.net/
+
+to download the .tar.gz file, or you can use easy_install::
+
+   easy_install Lyntin
+
+""",
+      license=LICENSE,
       author=AUTHOR,
       author_email=EMAIL,
-      license=LICENSE,
+      keywords="mud client",
       url=URL,
+      packages=find_packages(exclude=["ez_setup"]),
       scripts=SCRIPTS,
-      packages=['lyntin', 'lyntin.modules', 'lyntin.ui', 'scripts'],
-      long_description=
-"""Lyntin is a mud client framework written in Python which allows for 
-plugins, different user interfaces, and the use of Python on the command 
-line."""
-      )
+      include_package_data = False,
+      install_requires = [], # FIXME
+      classifiers = ["Development Status :: 5 - Production/Stable",
+                     "Intended Audience :: Developers",
+                     "Intended Audience :: End Users/Desktop",
+                     "License :: OSI Approved :: GNU General Public License 3.0 (GPL)",
+                     "Operating System :: OS Independent",
+                     "Programming Language :: Python",
+                     "Topic :: Games/Entertainment :: Multi-User Dungeons (MUD)",
+                     ],
+       )
